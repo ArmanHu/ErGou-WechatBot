784180fbcb57c6f4eac359ab0cd39d3281aa5d68
ArmanHu <109946679+ArmanHu@users.noreply.github.com>
2022-09-01 10:12:47 +0800

    Delete WechatBot directory

--- WechatBot/src/main/java/io/uouo/wechatbot/common/ThreadPoolConfig.java
+++
@@ -1,9 +0,0 @@
-package io.uouo.wechatbot.common;
-
-import java.util.concurrent.*;
-
-public class ThreadPoolConfig {
-
-    public static ExecutorService gamePool = Executors.newCachedThreadPool();
-
-}
--- WechatBot/src/main/java/io/uouo/wechatbot/webservice/weather/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <a href="http://www.webxml.com.cn/" target="_blank">WebXml.com.cn</a> <strong>����Ԥ�� Web ��������ÿ2.5Сʱ�����Զ�����һ�Σ�׼ȷ�ɿ������� 340 ����й���Ҫ���к� 60 ���������Ҫ���������ڵ�����Ԥ�����ݡ�</br>������Ԥ��Web Services�벻Ҫ�����κ���ҵĿ�ģ�������Ҫ��<a href="http://www.webxml.com.cn/zh_cn/contact_us.aspx" target="_blank">��ϵ����</a>����ӭ���������� QQ��8409035<br />ʹ�ñ�վ WEB ������ע�������ӱ�վ��http://www.webxml.com.cn/ ��л��ҵ�֧��</strong>��<br /><span style="color:#999999;">֪ͨ������Ԥ�� WEB ������ԭ��ʹ�õ�ַ http://www.onhap.com/WebServices/WeatherWebService.asmx �ģ���ĳ�����ʹ�õķ����ַ http://www.webxml.com.cn/WebServices/WeatherWebService.asmx ���������ü��ɡ�</span><br /><br />&nbsp;
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://WebXml.com.cn/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package io.uouo.wechatbot.webservice.weather;
